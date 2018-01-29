f3745f59f5d62cabc4f46546ed77de5caa340488
Bernie Mason <bernie.mason@gmail.com>
2018-01-29 07:48:13 +0000

    Generic update

--- LEISHMANIASIS/epro_scripts.sh
+++ LEISHMANIASIS/epro_scripts.sh
@@ -5,15 +5,21 @@
 LOGS_PATH=~/logs/dev/epro/
 FILE_PATH_BUILD_OUTPUT_EPRO_EVERYTHING=${LOGS_PATH}last_epro_build_all_output.txt
 FILE_PATH_BUILD_OUTPUT_EPRO_SPEECH=${LOGS_PATH}last_epro_build_speech_output.txt
+FILE_PATH_BUILD_OUTPUT_EPRO_DICTATION=${LOGS_PATH}last_epro_build_dictation_output.txt
 FILE_PATH_BUILD_OUTPUT_EPRO_METAPARSER=${LOGS_PATH}last_epro_build_metaparser_output.txt
+FILE_PATH_BUILD_OUTPUT_EPRO_FILEDROP=${LOGS_PATH}last_epro_build_filedrop_output.txt
 FILE_PATH_BUILD_OUTPUT_EPRO=${LOGS_PATH}last_epro_build_output.txt
 
 function epro-build() {
 	build-vs /c/Dev/epro/Epro.sln $FILE_PATH_BUILD_OUTPUT_EPRO
 }
 
-function epro-build-all() {
+function epro-build-everthing() {
 	build-vs /c/Dev/epro/Everything.sln $FILE_PATH_BUILD_OUTPUT_EPRO_EVERYTHING 
+}
+
+function epro-build-dictation() {
+	build-vs /c/Dev/epro/Bluewire.Dictation.sln $FILE_PATH_BUILD_OUTPUT_EPRO_DICTATION 
 }
 
 function epro-build-metaparser() {
@@ -24,6 +30,19 @@ function epro-build-speech() {
 	build-vs /c/Dev/epro/Bluewire.Speech.sln $FILE_PATH_BUILD_OUTPUT_EPRO_SPEECH 
 }
 
+function epro-build-filedrop() {
+	build-vs /c/Dev/epro-filedrop/Bluewire.FileDrop.sln $FILE_PATH_BUILD_OUTPUT_EPRO_METAPARSER 
+}
+
 function epro-build-logs() {
 	cd LOGS_PATH
+}
+
+function epro-servers(){
+	CURRENT_WD=$(PWD)
+	cd '/C/Dev/epro'
+	## ./ used to run via cmd.exe
+	./RUN_EPRO_SERVERS.bat
+
+	cd $CURRENT_WD
 }
