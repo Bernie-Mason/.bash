4ad049c65e761078415c7e19fd9375befa3c5efa
Bernie <bernie.mason@gmail.com>
2018-08-14 21:13:27 +0100

    Generic update

--- global/Scripts/Utilities/generic_update.sh
+++ global/Scripts/Utilities/generic_update.sh
@@ -14,18 +14,18 @@ function generic-update(){
 	local ISUPTODATE=$(git status | grep "Your branch is up-to-date with 'origin/master'.")
 	if [ -z "$ISUPTODATE" ];
 	then
-		git pull || die 1 ""
+		git pull || die 1 "Couldn't pull"
 	else
-		echo "Your branch is up-to-date with 'origin/master'."
+		echo "Y'all already up-to-date."
 	fi
 	local ISWORKINGTREECLEAN=$(git status | grep "nothing to commit, working tree clean")
 	if [ -z "$ISWORKINGTREECLEAN" ];
 	then
 		git add .
 		git commit -m 'Generic update'
-		git push || die 1 ""
+		git push || die 1 "Couldn't push"
 	else
-		echo "Nothing to commit, working tree clean."
+		echo "Nothing to commit yo, working tree clean."
 	fi
 	cd -
 }
