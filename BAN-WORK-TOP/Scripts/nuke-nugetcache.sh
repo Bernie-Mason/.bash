4fe7f47b03a385deeae77c7f050c3c7ac1e9ef4d
Bernard Mason <bernard.mason@thinksmartbox.com>
2021-04-05 17:09:09 +0100

    Update

--- BAN-WORK-TOP/Scripts/nuke-nugetcache.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-say() {
-	echo "$@" 1>&2 #redirect stdout to stderr
-}
-
-PACKAGE_CACHE='/c/users/Bernie/.nuget/packages/'
-say "Removing all files from ${PACKAGE_CACHE}"
-rm -rf ${PACKAGE_CACHE} || echo "Could not remove cache"
